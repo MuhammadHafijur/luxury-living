380a9cf02952cefd2dfa6f7e180dd3580da50c8e
MuhammadHafijur <hafijur.salah@gmail.com>
2022-01-09 18:53:42 +0600

    added testimonial section

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -3,6 +3,7 @@ import Navbar from '../../Shared/Navbar/Navbar';
 import Banner from '../Banner/Banner';
 import Projects from '../Projects/Projects';
 import Services from '../Services/Services';
+import Testimonials from '../Testimonials/Testimonials';
 
 const Home = () => {
     return (
@@ -11,6 +12,7 @@ const Home = () => {
             <Banner></Banner>
             <Projects></Projects>
             <Services></Services>
+            <Testimonials></Testimonials>
         </div>
     );
 };
---
+++ src/Pages/Home/Testimonials/Testimonials.js
@@ -0,0 +1,94 @@
+import React from "react";
+
+const Testimonials = () => {
+  return (
+    <div className="bg-gray-200">
+        <h1 className="text-3xl pt-24 pb-8 text-center font-bold text-gray-800">Testimonials</h1>
+      <div className="w-5/6 mx-auto flex flex-col md:flex-row gap-4 mb-8 md:mb-0 flex-between items-center p-8">
+        <div className="bg-white dark:bg-gray-800 w-72 shadow-lg mx-auto rounded-xl p-4">
+          <div className="flex items-center my-4">
+            <a href="#" className="block relative">
+              <img
+                alt="profil"
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQqkUYrITWyI8OhPNDHoCDUjGjhg8w10_HRqg&usqp=CAU"
+                className="mx-auto object-cover rounded-full h-10 w-10 "
+              />
+            </a>
+            <div className="flex flex-col ml-2 justify-between">
+              <span className="font-semibold text-gray-700 text-md">
+                Jean Miguel
+              </span>
+              <span className="dark:text-gray-400 text-xs flex items-center">
+                CEO, Manpol
+                <img src="/icons/rocket.svg" className="ml-2 h-4 w-4" />
+              </span>
+            </div>
+          </div>
+          <p className="text-gray-600 dark:text-white">
+            <span className="font-bold text-indigo-500 text-lg">“</span>
+            To get social media testimonials like these, keep your customers
+            engaged with your social media accounts by posting regularly
+            yourself
+            <span className="font-bold text-indigo-500 text-lg">”</span>
+          </p>
+        </div>
+        <div className="bg-white dark:bg-gray-800 w-72 shadow-lg mx-auto rounded-xl p-4">
+          <div className="flex items-center my-4">
+            <a href="#" className="block relative">
+              <img
+                alt="profil"
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQqkUYrITWyI8OhPNDHoCDUjGjhg8w10_HRqg&usqp=CAU"
+                className="mx-auto object-cover rounded-full h-10 w-10 "
+              />
+            </a>
+            <div className="flex flex-col ml-2 justify-between">
+              <span className="font-semibold text-gray-700 text-md">
+                Jean Miguel
+              </span>
+              <span className="dark:text-gray-400 text-xs flex items-center">
+                CEO, Manpol
+                <img src="/icons/rocket.svg" className="ml-2 h-4 w-4" />
+              </span>
+            </div>
+          </div>
+          <p className="text-gray-600 dark:text-white">
+            <span className="font-bold text-indigo-500 text-lg">“</span>
+            To get social media testimonials like these, keep your customers
+            engaged with your social media accounts by posting regularly
+            yourself
+            <span className="font-bold text-indigo-500 text-lg">”</span>
+          </p>
+        </div>
+        <div className="bg-white dark:bg-gray-800 w-72 shadow-lg mx-auto rounded-xl p-4">
+          <div className="flex items-center my-4">
+            <a href="#" className="block relative">
+              <img
+                alt="profil"
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQqkUYrITWyI8OhPNDHoCDUjGjhg8w10_HRqg&usqp=CAU"
+                className="mx-auto object-cover rounded-full h-10 w-10 "
+              />
+            </a>
+            <div className="flex flex-col ml-2 justify-between">
+              <span className="font-semibold text-gray-700 text-md">
+                Jean Miguel
+              </span>
+              <span className="dark:text-gray-400 text-xs flex items-center">
+                CEO, Manpol
+                <img src="/icons/rocket.svg" className="ml-2 h-4 w-4" />
+              </span>
+            </div>
+          </div>
+          <p className="text-gray-600 dark:text-white">
+            <span className="font-bold text-indigo-500 text-lg">“</span>
+            To get social media testimonials like these, keep your customers
+            engaged with your social media accounts by posting regularly
+            yourself
+            <span className="font-bold text-indigo-500 text-lg">”</span>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Testimonials;
